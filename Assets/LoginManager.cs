cab002aa2e9e400df7964e9fa44fdb8cfc1b26a8
Jeronimo Sanchez <jeronimo.sanchez@ort.edu.ar>
2024-05-27 16:03:25 -0300

    Update LoginManager.cs

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Cuando el usuario presione el botón BtnAceptar
+//debe escribirse un mensaje en la consola
+//si el texto del inputPassword coincide 
+//con una contraseña predefinida por ustedes 
+//escribir "Access Granted"
+//sino escribir "Access Denied"
 public class LoginManager : MonoBehaviour
 {
     // Start is called before the first frame update
